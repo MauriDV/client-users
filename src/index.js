710feaea9cf4f98fb2ce64bfb0a573eb572cc605
MauriDV <mdellevedove12@gmail.com>
2017-03-25 00:36:02 -0300

    test1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute,browserHistory} from 'react-router';
 import Header from '../components/Header';
-import App from './App';
 import './index.css';
 
 ReactDOM.render(
