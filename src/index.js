83551742d9a89134c1b8b635e249d1799461b3fb
MauriDV <mdellevedove12@gmail.com>
2017-03-25 01:10:18 -0300

    ni puta idea

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute,browserHistory} from 'react-router';
-import Header from '../components/Header';
+import Header from './components/Header';
 import './index.css';
 
 ReactDOM.render(
