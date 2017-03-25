70d69ac4fe8718b17b252c70a20894bd38849d23
MauriDV <mdellevedove12@gmail.com>
2017-03-25 01:27:29 -0300

    ni puta idea

--- src/components/Session.js
+++ src/components/Session.js
@@ -6,7 +6,7 @@ export default class Session extends React.Component{
     this.state = {usuario:{}}
   }
   componentWillMount(){
-    fetch('https://localhost:3000/api/users/session')
+    fetch('//localhost:3000/api/users/session')
       .then(function(response){
         return response.json()
       })
