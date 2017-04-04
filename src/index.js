579e9aa9e663cc859f0fa1b7422130c31724713b
MauriDV <mdellevedove12@gmail.com>
2017-04-03 22:09:45 -0300

    add routes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute,browserHistory} from 'react-router';
 import Header from './components/Header';
+import Home from './routes/Home';
 import './index.css';
 
 ReactDOM.render(
-  <Header/>,
-  document.getElementById('root')
+  (
+  	<Router history={browserHistory}>
+		<Route path="/" component={Header}>
+			<IndexRoute component={Home}/>
+		</Route>
+	</Router>
+  ),document.getElementById('root')
 );
